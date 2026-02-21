09f1587128123eb6f5ee425a23d8f823f2e5a0e8
Waitggg <kirillkorolt2589@gmail.com>
2026-02-21 13:03:31 +0300

    update: auth working!

---
+++ server/server/public/userHash.js
@@ -0,0 +1,13 @@
+export async function hs256(message) {
+    const secret = '52';
+    const enc = new TextEncoder();
+    const keyData = enc.encode(secret);
+    const msgData = enc.encode(message);
+    const cryptoKey = await crypto.subtle.importKey('raw', keyData, { name: 'HMAC', hash: 'SHA-256' }, false, ['sign']);
+    const signature = await crypto.subtle.sign('HMAC', cryptoKey, msgData);
+    const base64 = btoa(String.fromCharCode(...new Uint8Array(signature)))
+        .replace(/\+/g, '-')
+        .replace(/\//g, '_')
+        .replace(/=+$/, '');
+    return base64;
+}
