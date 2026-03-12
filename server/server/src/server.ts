30f3df071db226b8cbaaaf9c333ebbd731f341f7
Artem <140013348+Archangelo4ek@users.noreply.github.com>
2026-03-12 08:16:35 +0300

    исправленный server.ts

--- server/server/src/server.ts
+++ server/server/src/server.ts
@@ -1,32 +1,25 @@
-import express, { Application, Request, Response} from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import path from 'path';
-import { createUser } from './controllers/auth_controller';
-import { User } from './types/users';
 import { authMiddleware } from './services/auth_validator';
-import { login, register, profile } from './routes/server_routes';
+import { login, register, profile, getGames } from './routes/server_routes';
 
 const app: Application = express();
 const PORT = 3000;
 
 app.use(cors());
-
 app.use(express.json());
-
-app.use(express.urlencoded({ extended: true })); 
-
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.post('/api/register', register);
-
 app.post('/api/login', login);
-
 app.get('/api/profile', authMiddleware, profile);
-
+app.get('/api/games', getGames);
 app.get('/api/products', (req: Request, res: Response) => {
-  res.send({data: [{id: 1, title: "kbp"}]});
+    res.send({ data: [{ id: 1, title: "kbp" }] });
 });
 
 app.listen(PORT, () => {
-    console.log('enjebado');
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
